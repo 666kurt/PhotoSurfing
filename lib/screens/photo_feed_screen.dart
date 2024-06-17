f4542ad856ef4e209c005cd39054446d8863d107
Maxim <incentivedie@gmail.com>
2024-06-17 14:26:27 +0400

    fixed bugs

--- lib/screens/photo_feed_screen.dart
+++ lib/screens/photo_feed_screen.dart
@@ -1,13 +1,41 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:photo_surfing/widgets/widgets.dart';
 
 class PhotoFeedScreen extends StatelessWidget {
-  const PhotoFeedScreen({super.key});
+  final TextEditingController controller = TextEditingController();
+
+  PhotoFeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Photo Feed'),
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Photo Feed'),
+          bottom: PhotoFeedTabBar(),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              children: [
+                PhotoFeedSearchBar(controller: controller),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      AllPhotoGrid(),
+                      FavoritesAuthorsGrid(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,9 @@
-export 'custom_text_field.dart';
-export 'custom_button.dart';
-export 'custom_row_button.dart';
-export 'register_image_placeholder.dart';
+export 'photo_feed_widgets/all_photo_grid.dart';
+export 'photo_feed_widgets/photo_feed_searchbar.dart';
+export 'photo_feed_widgets/favorites_authors_grid.dart';
+export 'photo_feed_widgets/photo_feed_tabbar.dart';
+
+export 'auth_widgets/custom_button.dart';
+export 'auth_widgets/custom_row_button.dart';
+export 'auth_widgets/custom_text_field.dart';
+export 'auth_widgets/register_image_placeholder.dart';
