3c90081e073d15ce76ac3a66b8c0bdb72ef5f3a4
Maxim <incentivedie@gmail.com>
2024-06-21 17:10:56 +0400

    Added tabbar and calendar

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class AccountScreen extends StatelessWidget {
-  const AccountScreen({super.key});
+class AccountScreen extends StatefulWidget {
+  AccountScreen({super.key});
+
+  @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Account'),
-      ),
+      appBar: AppBar(title: Text('Account')),
       body: TableCalendar(
-        firstDay: DateTime.utc(2020, 1, 1),
+        firstDay: DateTime.utc(2022, 1, 1),
         lastDay: DateTime.utc(2030, 1, 1),
-        focusedDay: DateTime.now(),
-        calendarFormat: CalendarFormat.month,
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        onFormatChanged: (format) {
+          setState(() {
+            _calendarFormat = format;
+          });
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _focusedDay = focusedDay;
+            _selectedDay = selectedDay;
+          });
+        },
       ),
     );
   }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:photo_surfing/screens/photo_feed_screen.dart';
 import 'package:photo_surfing/screens/screens.dart';
 
 class MainScreen extends StatefulWidget {
@@ -12,12 +13,6 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
 
-  static List<Widget> _screenOptions = [
-    PhotoFeedScreen(),
-    Center(child: Text('add')),
-    AccountScreen(),
-  ];
-
   void _onItemTapped(int index) {
     setState(() {
       if (_selectedIndex == index) {
@@ -28,37 +23,33 @@ class _MainScreenState extends State<MainScreen> {
     });
   }
 
+  static List<Widget> _screenOptions = [
+    PhotoFeedScreen(),
+    Center(child: Text('add')),
+    AccountScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screenOptions.elementAt(_selectedIndex),
       bottomNavigationBar: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: GNav(
-            hoverColor: Color.fromRGBO(0, 98, 255, 0.5),
-            color: Color.fromRGBO(7, 7, 12, 1),
-            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            tabBackgroundColor: Color.fromRGBO(0, 98, 255, 0.5),
-            activeColor: Colors.white,
-            gap: 8,
-            tabs: [
-              GButton(
-                icon: Icons.photo_library,
-                text: 'Photo Feed',
-              ),
-              GButton(
-                icon: Icons.add_box,
-                text: 'Add',
-              ),
-              GButton(
-                icon: Icons.account_circle,
-                text: 'Account',
-              ),
-            ],
-            onTabChange: _onItemTapped,
-            selectedIndex: _selectedIndex,
-          ),
+        minimum: EdgeInsets.symmetric(horizontal: 16),
+        child: GNav(
+          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          gap: 8,
+          tabBackgroundColor: Color.fromRGBO(0, 98, 255, 0.5),
+          hoverColor: Color.fromRGBO(0, 98, 255, 0.5),
+          activeColor: Colors.white,
+          duration: Duration(milliseconds: 200),
+          color: Color.fromRGBO(7, 7, 12, 1),
+          tabs: [
+            GButton(icon: Icons.photo_library, text: 'Photo Feed'),
+            GButton(icon: Icons.add_box, text: 'Add'),
+            GButton(icon: Icons.account_circle, text: 'Account'),
+          ],
+          onTabChange: _onItemTapped,
+          selectedIndex: _selectedIndex,
         ),
       ),
     );
